4d1683ed2f31de2afd0f79a04c6c6345975c9903
Herbert Straub <herbert@linuxhacker.at>
2012-01-28 20:27:44 +0100

    cleanup import

--- src/at/linuxhacker/weightassistant/WeightAssistantActivity.java
+++ src/at/linuxhacker/weightassistant/WeightAssistantActivity.java
@@ -2,7 +2,6 @@ package at.linuxhacker.weightassistant;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
