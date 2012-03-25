940199b39f11c3dd12254aaf6d32ce46c8cba6a1
Herbert Straub <herbert@linuxhacker.at>
2012-03-25 13:54:15 +0200

    Working Graph with weight, 3. and 5. order

--- src/at/linuxhacker/weightassistant/WeightOverviewGraph.java
+++ src/at/linuxhacker/weightassistant/WeightOverviewGraph.java
@@ -2,14 +2,16 @@ package at.linuxhacker.weightassistant;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.chart.PointStyle;
 import org.achartengine.model.TimeSeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
@@ -25,6 +27,9 @@ public class WeightOverviewGraph {
 	private SQLiteDatabase db;
 	private XYMultipleSeriesRenderer renderer;
 	private XYMultipleSeriesDataset dataset;
+	private List<MeasuringPoint> measurmentSeries;
+	private List<Double> series3;
+	private List<Double> series5;
 
 	public Intent execute(Context context) {
 		this.dbHelper = new DbHelper( context );
@@ -33,16 +38,13 @@ public class WeightOverviewGraph {
 		this.renderer = new XYMultipleSeriesRenderer( );
 
 		this.readDB( );
+		this.series3 = this.calcAverage( 3 );
+		this.series5 = this.calcAverage( 5 );
 		setChartSettings( );
-		setFirstSerie( );
-		setSecondSerie( );
-		setThirdSerie( );
-		//setMinMaxChartSettings( );
+		setSeries( );
 		return ChartFactory.getTimeChartIntent(context, this.dataset,
 				this.renderer, "yyyy-MM-dd");
-
 	}
-
 	
 	protected void setChartSettings( ) {
 		this.renderer.setChartTitle( "Mein Gewicht" );
@@ -60,57 +62,68 @@ public class WeightOverviewGraph {
 		this.renderer.setMargins(new int[] { 20, 40, 20, 30 });
 	}
 	
-	protected void setFirstSerie( ) {
+	protected void setSeries( ) {
+		TimeSeries timeSeries = ( TimeSeries ) new TimeSeries( "Gewicht" );
+		TimeSeries timeSeries3 = ( TimeSeries ) new TimeSeries( "3. Ordnung" );
+		TimeSeries timeSeries5 = ( TimeSeries ) new TimeSeries( "5. Ordnung" );
+		
+		int length3 = this.series3.size( );
+		int length5 = this.series5.size( );
+		int maxLength = this.measurmentSeries.size( );
+		for ( int i = 0; i < maxLength; i++ ) {
+			int pos3 = length3 - maxLength + i;
+			int pos5 = length5 - maxLength + i;
+			if ( pos5 < 0 || pos3 < 0 ) {
+				continue;
+			}
+			Date time = this.measurmentSeries.get( i ).getDate( );
+			timeSeries.add( time, this.measurmentSeries.get( i ).getWeight( ) );
+			timeSeries3.add( time, this.series3.get( pos3 ) );
+			timeSeries5.add( time, this.series5.get( pos5 ) );
+		}
+		
+		this.dataset.addSeries( timeSeries );
 		XYSeriesRenderer r = new XYSeriesRenderer( );
 		r.setColor( Color.CYAN );
 		r.setPointStyle( PointStyle.TRIANGLE );
-		this.renderer.addSeriesRenderer( r );		
-	}
-	
-	protected void setSecondSerie( ) {
-		TimeSeries series = ( TimeSeries ) this.dataset.getSeriesAt( 0 );
-		TimeSeries series3 = new TimeSeries( "3. Ordnung" );
-		series3.add( series.getX( 0 ), series.getY( 0 ) );
-		int length = series.getItemCount( );
-		for ( int i = 1; i < length - 1; i++ ) {
-			double y = series.getY( i -1 ) + series.getY( i ) + series.getY( i + 1 );
-			y = y / 3;
-			series3.add( series.getX( i ), y );
-		}
-		series3.add(series.getX( length -1  ), series.getY( length -1 ) );
-		this.dataset.addSeries( series3 );
-		XYSeriesRenderer r = new XYSeriesRenderer( );
+		this.renderer.addSeriesRenderer( r );
+		
+		this.dataset.addSeries( timeSeries3 );
+		r = new XYSeriesRenderer( );
 		r.setColor( Color.RED );
 		r.setPointStyle( PointStyle.DIAMOND );
-		this.renderer.addSeriesRenderer( r );
-	}
-
-	protected void setThirdSerie( ) {
-		TimeSeries series = ( TimeSeries ) this.dataset.getSeriesAt( 0 );
-		TimeSeries series5 = new TimeSeries( "5. Ordnung" );
-		series5.add( series.getX( 0 ), series.getY( 0 ) );
-		series5.add( series.getX( 1 ), series.getY( 1 ) );
-		int length = series.getItemCount( );
-		for ( int i = 2; i < length - 2; i++ ) {
-			double y = series.getY( i - 2 ) + series.getY( i - 1 ) + series.getY( i )
-					+ series.getY( i + 1 ) + series.getY( i + 2 );
-			y = y / 5;
-			series5.add( series.getX( i ), y );
-		}
-		series5.add(series.getX( length -2  ), series.getY( length -2 ) );
-		series5.add(series.getX( length -1  ), series.getY( length -1 ) );
-		this.dataset.addSeries( series5 );
-		XYSeriesRenderer r = new XYSeriesRenderer( );
+		this.renderer.addSeriesRenderer( r );		
+		
+		this.dataset.addSeries( timeSeries5 );
+		r = new XYSeriesRenderer( );
 		r.setColor( Color.YELLOW );
 		r.setPointStyle( PointStyle.TRIANGLE );
 		r.setLineWidth( 5 );
-		this.renderer.addSeriesRenderer( r );
+		this.renderer.addSeriesRenderer( r );		
+	}
+	
+	protected List<Double> calcAverage( int order ) {
+		List<Double> series = new ArrayList<Double>( );
+		if ( this.measurmentSeries.size() < order ) {
+			return series;
+		}
+		
+		for ( int i = this.measurmentSeries.size( ) -1; i >= order -1; i-- ) {
+			double average = this.measurmentSeries.get( i ).getWeight( );
+			for ( int j = 1; j < order; j++ ) {
+				average += this.measurmentSeries.get( i - j ).getWeight( );
+			}
+			average = average / order;
+			series.add( average );
+		}
+		Collections.reverse( series );
+		return series;
 	}
 
 	protected void readDB( ) {
+		this.measurmentSeries = new ArrayList<MeasuringPoint>( );
 		SimpleDateFormat dateFormat = new SimpleDateFormat( "yyyy-MM-dd" );
 		Date myDate = null;
-		TimeSeries series = new TimeSeries( new String( "Gewicht" ) );
 		
 		try {
 			Cursor cur = this.db.query( this.dbHelper.TABLE, 
@@ -122,26 +135,12 @@ public class WeightOverviewGraph {
 			for( int i = 0; i < rowCount; i++ ) {
 				String tmp =  cur.getString( 1 );
 				myDate = dateFormat.parse( tmp );
-				//myDate = dateFormat.parse( cur.getString( 1 ) );
-				series.add( myDate, cur.getDouble( 2 ) );
+				this.measurmentSeries.add( new MeasuringPoint( myDate, cur.getDouble( 2 ) ) );
 				cur.moveToNext( );
 			}
 		} catch ( Exception e ) {
 			Exception y;
 			y = e;
 		}
-		this.dataset.addSeries( series );
 	}
-	
-	// Ab da nur mehr obselete Methoden
-	protected void setMinMaxChartSettings( ) {
-		// Ist eigentlich gar nicht notwendig, in dieser Form
-		TimeSeries series = ( TimeSeries ) this.dataset.getSeriesAt( 0 );
-
-		this.renderer.setXAxisMin( series.getMinX( ) );
-		this.renderer.setXAxisMax( series.getMaxX( ) );
-		this.renderer.setYAxisMin( series.getMinY( ) );
-		this.renderer.setYAxisMax( series.getMaxY( ) );		
-	}
-
 }
