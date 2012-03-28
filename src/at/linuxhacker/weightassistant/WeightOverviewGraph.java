887a1e3c02e660a6b864282fd700d66e45bca69c
Herbert Straub <herbert@linuxhacker.at>
2012-03-28 11:37:57 +0200

    WeightOverviewGraph   - with 7. and 14. order   - Increase margin button value

--- src/at/linuxhacker/weightassistant/WeightOverviewGraph.java
+++ src/at/linuxhacker/weightassistant/WeightOverviewGraph.java
@@ -23,15 +23,15 @@ public class WeightOverviewGraph {
 	private XYMultipleSeriesRenderer renderer;
 	private XYMultipleSeriesDataset dataset;
 	private WeightMeasurmentSeries weightMeasurmentSeries;
+	private List<Double> series2;
 	private List<Double> series3;
-	private List<Double> series5;
 
 	public Intent execute(Context context) {
 		this.dataset = new XYMultipleSeriesDataset();
 		this.renderer = new XYMultipleSeriesRenderer( );
 
-		this.series3 = this.calcAverage( 3 );
-		this.series5 = this.calcAverage( 5 );
+		this.series2 = this.calcAverage( 7 );
+		this.series3 = this.calcAverage( 14 );
 		setChartSettings( );
 		setSeries( );
 		return ChartFactory.getTimeChartIntent(context, this.dataset,
@@ -55,42 +55,42 @@ public class WeightOverviewGraph {
 		this.renderer.setLegendTextSize( 23 );
 		this.renderer.setPointSize(5f);
 		// top, left, button, right
-		this.renderer.setMargins(new int[] { 20, 40, 20, 30 });
+		this.renderer.setMargins(new int[] { 20, 40, 40, 30 });
 	}
 	
 	protected void setSeries( ) {
-		TimeSeries timeSeries = ( TimeSeries ) new TimeSeries( "Gewicht" );
-		TimeSeries timeSeries3 = ( TimeSeries ) new TimeSeries( "3. Ordnung" );
-		TimeSeries timeSeries5 = ( TimeSeries ) new TimeSeries( "5. Ordnung" );
+		TimeSeries timeSeries1 = ( TimeSeries ) new TimeSeries( "Gewicht" );
+		TimeSeries timeSeries2 = ( TimeSeries ) new TimeSeries( "7. Ordnung" );
+		TimeSeries timeSeries3 = ( TimeSeries ) new TimeSeries( "14. Ordnung" );
 		
+		int length2 = this.series2.size( );
 		int length3 = this.series3.size( );
-		int length5 = this.series5.size( );
 		int maxLength = this.weightMeasurmentSeries.measurmentSeries.size( );
 		for ( int i = 0; i < maxLength; i++ ) {
+			int pos2 = length2 - maxLength + i;
 			int pos3 = length3 - maxLength + i;
-			int pos5 = length5 - maxLength + i;
-			if ( pos5 < 0 || pos3 < 0 ) {
+			if ( pos3 < 0 || pos2 < 0 ) {
 				continue;
 			}
 			Date time = this.weightMeasurmentSeries.measurmentSeries.get( i ).getDate( );
-			timeSeries.add( time, this.weightMeasurmentSeries.measurmentSeries.get( i ).getWeight( ) );
+			timeSeries1.add( time, this.weightMeasurmentSeries.measurmentSeries.get( i ).getWeight( ) );
+			timeSeries2.add( time, this.series2.get( pos2 ) );
 			timeSeries3.add( time, this.series3.get( pos3 ) );
-			timeSeries5.add( time, this.series5.get( pos5 ) );
 		}
 		
-		this.dataset.addSeries( timeSeries );
+		this.dataset.addSeries( timeSeries1 );
 		XYSeriesRenderer r = new XYSeriesRenderer( );
 		r.setColor( Color.CYAN );
 		r.setPointStyle( PointStyle.TRIANGLE );
 		this.renderer.addSeriesRenderer( r );
 		
-		this.dataset.addSeries( timeSeries3 );
+		this.dataset.addSeries( timeSeries2 );
 		r = new XYSeriesRenderer( );
 		r.setColor( Color.RED );
 		r.setPointStyle( PointStyle.DIAMOND );
 		this.renderer.addSeriesRenderer( r );		
 		
-		this.dataset.addSeries( timeSeries5 );
+		this.dataset.addSeries( timeSeries3 );
 		r = new XYSeriesRenderer( );
 		r.setColor( Color.YELLOW );
 		r.setPointStyle( PointStyle.TRIANGLE );
